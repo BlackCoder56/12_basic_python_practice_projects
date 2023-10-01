5f79838de112b83ae63fa5445ec73e33fda80265
Elly <blackcoda56@gmail.com>
2023-10-01 21:19:26 +0300

    init commit

---
+++ madlips_01/madlips.py
@@ -0,0 +1,13 @@
+
+adjective_1 = input('Enter adjective: ')
+adjective_2 = input('Enter adjective 2: ')
+famous_tech_person1 = input('Enter famous persion 1:')
+famous_tech_person2 = input('Enter famous persion 2:')
+famous_tech_person3 = input('Enter famous persion 3:')
+famous_tech_person4 = input('Enter famous persion 4:')
+country = input('Enter your country: ')
+course = input('Enter programming course: ')
+
+madlip = f"\nProgramming is {adjective_1}. Most programmers say to be a good programmer you have to {adjective_2}.\n Young programmers are always motivated by {famous_tech_person1}, {famous_tech_person2}, {famous_tech_person3} & {famous_tech_person4} and forget to be motivated by themselves.\nIn {country}, {course} is not really appriated due to the under development and vast computer iliteracy!\n"
+
+print(madlip)
