fc02d0b1655b9ef2da0ce972037c1b0dcd2f5c0d
Elly <blackcoda56@gmail.com>
2023-10-04 11:18:52 +0300

    guessing

--- guess_the_number_02/guess.py
+++ guess_the_number_02/guess.py
@@ -1,2 +1,15 @@
+import random
 
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0    
+    while guess != random_number:
+        guess = int(input(f'Guess number between 1 and {x}: '))
+        if guess > random_number:
+            print('The guessed number is higher than random number')
+        elif guess < random_number:
+            print('The guessed number is lower than random number')    
+    print(f'You guess the {random_number} corrently, yeay!')
     
+guess(100)
+        
