0c09cf6d5523e4c7a460ca74d590ed1e21e51bd7
Elly <blackcoda56@gmail.com>
2023-10-08 13:49:02 +0300

    initializing the functions

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,3 +1,16 @@
+import random
 from words import words
+import string
 
-print(words)
+def get_valid_word(words):
+    word = random.choice(words) # gets initial random word
+    while '-' in word or ' ' in word: # iterates whenever the word contains dash[-] or space
+        word = random.choice(words) # gets new random number
+    
+    return word # returns valid word
+
+def hangman():
+    word = get_valid_word(words) # getting valid word
+    word_letter = set(word) # storing valid word in set
+    alphabet = set(string.ascii_uppercase) # dictionary alphabets
+    used_letter = set() # letters user already guessed
